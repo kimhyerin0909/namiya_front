1dcc326827cd1913da97dd788ab9965866b81760
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-05 14:56:39 +0900

    Chore - color set

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  AUTH_BACKGROUND: "linear-gradient(180deg, #2B4159 0%, #A87E90 100%)",
+  MAIN_BACKGROUND: "linear-gradient(180deg, #2B4159 0%, #3C3355 100%)",
+
+  LIGHT_YELLOW: "#FFFCEE",
+  YELLOW: "#FCF2BF",
+  YELLOW_DARK: "#DBCC7F",
+  NIGHT_BLUE: "#2B4159",
+
+  GREEN: "#1C7922",
+  GREEN_DARK: "#16621B",
+
+  RED: "#AB3535",
+  RED_DARK: "#8B2929",
+};
