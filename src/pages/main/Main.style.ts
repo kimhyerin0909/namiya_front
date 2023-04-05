e964754f881fcc4808b445c8124aa9cda50b99e3
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-05 19:57:26 +0900

    Feat - 잡화점 열고 닫기

---
+++ src/pages/main/Main.style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const StoreBox = styled.div`
+  position: absolute;
+  display: flex;
+  bottom: 5px;
+  left: 4%;
+  height: 500px;
+  align-items: flex-end;
+  justify-content: center;
+  object-fit: contain;
+`;
+
+export const Store = styled.img`
+  height: 100%;
+`;
+
+export const Door = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  bottom: 55px;
+  z-index: 2;
+  left: calc(4% + 120px);
+  width: 120px;
+  height: 145px;
+`;
+
+export const toMyStoreBtn = styled.button`
+  width: 100%;
+  font-size: 12px;
+  word-break: keep-all;
+  border: none;
+  background: rgb(255, 255, 255, 0);
+`;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import * as S from "./Main.style";
 
 export const Main = () => {
-  return <div>Main</div>;
+  const [storeImg, setStoreImg] = useState("close");
+  return (
+    <div>
+      <S.StoreBox>
+        <S.Door onMouseOver={() => setStoreImg("open")} onMouseLeave={() => setStoreImg("close")}>
+          {storeImg === "open" && <S.toMyStoreBtn>김혜린의 잡화점 들어가기</S.toMyStoreBtn>}
+        </S.Door>
+        <S.Store src={`/assets/store/${storeImg}-1.svg`} />
+      </S.StoreBox>
+    </div>
+  );
 };
