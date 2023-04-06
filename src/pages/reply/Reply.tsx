435932fcd022dcbf07b5caadc57bfcf0dd548daf
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-06 17:16:53 +0900

    Set - default component

---
+++ src/pages/reply/Reply.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Reply = () => {
+  return <div>Reply</div>;
+};
---
+++ src/pages/yours/Yours.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Yours = () => {
+  return <div>Yours</div>;
+};
