3674f865aadd96e8e8e28fa121f03e0462ae9a51
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-13 15:56:43 +0900

    Feat - signup api

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -1,6 +1,11 @@
-import { LOGIN_USER } from "@/types/auth";
+import { USER } from "@/types/auth";
 import customAxios from "../customAxios";
+import { refreshTokenHeader } from "@/util/auth/headers";
 
-export const login = async (request: LOGIN_USER) => {
+export const login = async (request: USER) => {
   return (await customAxios.post("/auth/login", request)).data;
+};
+
+export const signup = async (request: USER) => {
+  return (await customAxios.post("/auth/signup", request)).data;
 };
